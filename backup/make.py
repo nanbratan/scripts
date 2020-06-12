62b9aa3a07cd487a644e5d94f0c75bb84a0b5abf
nanbratan <morfemi9498@gmail.com>
2020-06-13 02:38:12 +0300

    remove class. it`s useless in that case

--- backup/make.py
+++ backup/make.py
@@ -14,40 +14,38 @@ as_root = 'pkexec'
 rsync_cmd = 'rsync'
 
 
-class BackupHadler:
-    @staticmethod
-    def get_current_date():
-        current_day = datetime.now().day
-        current_month = datetime.now().month
-        current_year = datetime.now().year
+def get_snap_path():
+    current_day = datetime.now().day
+    current_month = datetime.now().month
+    current_year = datetime.now().year
 
-        return f'{current_day}-{current_month}-{current_year}'
+    current_date = f'{current_day}-{current_month}-{current_year}'
 
-    def create_snap_dir(self):
-        current_date = BackupHadler.get_current_date()
-        snap_path = f'{backup_path}/{current_date}'
-
-        if os.path.exists(snap_path):
-            shutil.rmtree(snap_path)
-
-        os.mkdir(snap_path)
-
-    def make(self):
-        current_date = BackupHadler.get_current_date()
-        snap_path = f'{backup_path}/{current_date}'
-
-        self.create_snap_dir()
-
-        subprocess.run([
-            as_root,
-            rsync_cmd,
-            options,
-            f"--exclude-from={pwd}/exclude_dirs.txt",
-            root_path,
-            snap_path,
-        ])
+    return f'{backup_path}/{current_date}'
 
 
-backup_handler = BackupHadler()
+def create_snap_dir():
+    snap_path = get_snap_path
 
-backup_handler.make()
+    if os.path.exists(snap_path):
+        shutil.rmtree(snap_path)
+
+    os.mkdir(snap_path)
+
+
+def make_backup():
+    snap_path = get_snap_path()
+
+    create_snap_dir()
+
+    subprocess.run([
+        as_root,
+        rsync_cmd,
+        options,
+        f"--exclude-from={pwd}/exclude_dirs.txt",
+        root_path,
+        snap_path,
+    ])
+
+
+make_backup()
